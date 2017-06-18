29cee037010d4c1f05781dd8f7f24cc5f51837c7
micromi <484390775@qq.com>
2017-06-19 01:47:46 +0800

    initial commit

---
+++ 05/src/js/index.js
@@ -0,0 +1,24 @@
+var React = require('react');
+var ReactDOM =require('react-dom');
+
+/*ReactDOM.render(
+  <h1>Hello World!</h1>,
+  document.getElementById('example')
+);*/
+
+var ExampleApplication = React.createClass({
+  render: function() {
+    var elapsed = Math.round(this.props.elapsed  / 100);
+    var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+    var message =
+      'React has been successfully running for ' + seconds + ' seconds.';
+    return <p>{message}</p>;
+  }
+});
+var start = new Date().getTime();
+setInterval(function() {
+  ReactDOM.render(
+    <ExampleApplication elapsed={new Date().getTime() - start} />,
+    document.getElementById('example')
+  );
+}, 50);
---
+++ README.md
@@ -0,0 +1,16 @@
+# React-GettingStarted
+
+Imooc-React.js入门基础与案例开发
+
+> A React Getting Started Project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+webpack-dev-server --contentBase src --inline --hot --progress --color
+
+```
